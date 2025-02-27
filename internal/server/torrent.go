60bd86f8e9990b5e26be061be23c7a2f531e2140
Simon Elsbrock <simon@iodev.org>
2025-02-27 22:26:08 +0100

    feat(rpc): file tracking for *arr

--- internal/server/torrent.go
+++ internal/server/torrent.go
@@ -6,8 +6,23 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/elsbrock/go-putio"
 	"github.com/elsbrock/plundrio/internal/log"
 )
+
+// findTransferByHash finds a transfer by its hash string
+func (s *Server) findTransferByHash(hash string) (*putio.Transfer, error) {
+	transfers, err := s.client.GetTransfers()
+	if err != nil {
+		return nil, err
+	}
+	for _, t := range transfers {
+		if t.Hash == hash {
+			return t, nil
+		}
+	}
+	return nil, fmt.Errorf("transfer not found with hash: %s", hash)
+}
 
 // handleTorrentAdd processes torrent-add requests
 func (s *Server) handleTorrentAdd(args json.RawMessage) (interface{}, error) {
@@ -67,20 +82,30 @@ func (s *Server) handleTorrentAdd(args json.RawMessage) (interface{}, error) {
 			Str("magnet", name).
 			Int64("folder_id", s.cfg.FolderID).
 			Msg("Magnet link added")
+
+		// Return success response
+		return map[string]interface{}{
+			"torrent-added": map[string]interface{}{},
+		}, nil
 	}
 
 	// Return success response
 	return map[string]interface{}{
-		"torrent-added": map[string]interface{}{
-			"id":         0, // Put.io doesn't use transmission IDs
-			"name":       name,
-			"hashString": "",
-		},
+		"torrent-added": map[string]interface{}{},
 	}, nil
 }
 
 // handleTorrentGet processes torrent-get requests
 func (s *Server) handleTorrentGet(args json.RawMessage) (interface{}, error) {
+	var params struct {
+		IDs    []string `json:"ids"`
+		Fields []string `json:"fields"`
+	}
+
+	if err := json.Unmarshal(args, &params); err != nil {
+		return nil, fmt.Errorf("invalid arguments: %w", err)
+	}
+
 	transfers, err := s.client.GetTransfers()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get transfers: %w", err)
@@ -93,18 +118,38 @@ func (s *Server) handleTorrentGet(args json.RawMessage) (interface{}, error) {
 		if t.SaveParentID != s.cfg.FolderID {
 			continue
 		}
+
+		// Filter by IDs if specified
+		if len(params.IDs) > 0 {
+			found := false
+			for _, id := range params.IDs {
+				if id == t.Hash {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
 		status := s.mapPutioStatus(t.Status)
 		torrents = append(torrents, map[string]interface{}{
-			"id":           t.Hash,
-			"name":         t.Name,
-			"status":       status,
-			"downloadDir":  s.cfg.TargetDir,
-			"percentDone":  float64(t.PercentDone) / 100.0,
-			"rateDownload": t.DownloadSpeed,
-			"rateUpload":   t.UploadSpeed,
-			"uploadRatio":  float64(t.Uploaded) / float64(t.Size),
-			"error":        t.ErrorMessage != "",
-			"errorString":  t.ErrorMessage,
+			"id":             t.ID,
+			"hashString":     t.Hash,
+			"name":           t.Name,
+			"status":         status,
+			"downloadDir":    s.cfg.TargetDir,
+			"totalSize":      t.Size,
+			"leftUntilDone":  int64(t.Size) - t.Downloaded,
+			"uploadedEver":   t.Uploaded,
+			"downloadedEver": t.Downloaded,
+			"percentDone":    float64(t.PercentDone) / 100.0,
+			"rateDownload":   t.DownloadSpeed,
+			"rateUpload":     t.UploadSpeed,
+			"uploadRatio":    float64(t.Uploaded) / float64(t.Size),
+			"error":          t.ErrorMessage != "",
+			"errorString":    t.ErrorMessage,
 		})
 	}
 
@@ -116,25 +161,47 @@ func (s *Server) handleTorrentGet(args json.RawMessage) (interface{}, error) {
 // handleTorrentRemove processes torrent-remove requests
 func (s *Server) handleTorrentRemove(args json.RawMessage) (interface{}, error) {
 	var params struct {
-		IDs             []int64 `json:"ids"`
-		DeleteLocalData bool    `json:"delete-local-data"`
+		IDs             []string `json:"ids"`
+		DeleteLocalData bool     `json:"delete-local-data"`
 	}
 
 	if err := json.Unmarshal(args, &params); err != nil {
 		return nil, fmt.Errorf("invalid arguments: %w", err)
 	}
 
-	for _, id := range params.IDs {
-		if err := s.client.DeleteTransfer(id); err != nil {
+	for _, hash := range params.IDs {
+		transfer, err := s.findTransferByHash(hash)
+		if err != nil {
 			log.Error("rpc").
 				Str("operation", "torrent-remove").
-				Int64("transfer_id", id).
+				Str("hash", hash).
+				Err(err).
+				Msg("Failed to find transfer")
+			continue
+		}
+
+		// Delete the files of the transfer
+		if err := s.client.DeleteFile(transfer.FileID); err != nil {
+			log.Error("rpc").
+				Str("operation", "torrent-remove").
+				Str("hash", hash).
+				Int64("transfer_id", transfer.ID).
+				Err(err).
+				Msg("Failed to delete transfer files")
+		}
+
+		if err := s.client.DeleteTransfer(transfer.ID); err != nil {
+			log.Error("rpc").
+				Str("operation", "torrent-remove").
+				Str("hash", hash).
+				Int64("transfer_id", transfer.ID).
 				Err(err).
 				Msg("Failed to delete transfer")
 		} else {
 			log.Info("rpc").
 				Str("operation", "torrent-remove").
-				Int64("transfer_id", id).
+				Str("hash", hash).
+				Int64("transfer_id", transfer.ID).
 				Bool("delete_local_data", params.DeleteLocalData).
 				Msg("Transfer removed")
 		}
