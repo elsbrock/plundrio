9504a03e3e5cbcc6b56466fb625c7bd9d70d56ce
Simon Elsbrock <simon@iodev.org>
2025-03-29 22:57:33 +0100

    chore: next release is v0.10.1

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     let
       pname = "plundrio";
-      version = "0.10.0";
+      version = "0.10.1";
       description = "A Put.io integration for *arr applications";
       maintainer = {
         name = "Simon Elsbrock";
